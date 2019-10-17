8a33a7594ef24e0b998f8f8e43cf8d4c5de00c68
Fabio Maciel <fabio.souza@c6bank.com>
2019-10-17 19:07:40 -0300

    add GradingStudents solution

---
+++ src/main/kotlin/io/github/fabiomaciel/GradingStudents.kt
@@ -0,0 +1,32 @@
+package io.github.fabiomaciel
+
+/*
+ * Complete the 'gradingStudents' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY grades as parameter.
+ */
+
+fun gradingStudents(grades: Array<Int>): Array<Int> {
+    return grades.map {
+        if(it >= 38 && (it % 5) in 3..4){
+            it + (5 - it % 5)
+        }else{
+            it
+        }
+    }.toTypedArray()
+}
+
+fun main(args: Array<String>) {
+    val gradesCount = readLine()!!.trim().toInt()
+
+    val grades = Array<Int>(gradesCount, { 0 })
+    for (i in 0 until gradesCount) {
+        val gradesItem = readLine()!!.trim().toInt()
+        grades[i] = gradesItem
+    }
+
+    val result = gradingStudents(grades)
+
+    println(result.joinToString("\n"))
+}
