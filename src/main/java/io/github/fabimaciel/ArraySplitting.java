68ab3807ac39e5ab5afdda4a57a2433169e71d8b
Fabio Maciel <macielfsouza@gmail.com>
2020-02-07 18:21:50 -0300

    added some new challenge answers

---
+++ src/main/java/io/github/fabimaciel/ArraySplitting.java
@@ -0,0 +1,28 @@
+package io.github.fabimaciel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArraySplitting {
+    public static int splitIntoTwo(List<Integer> arr) {
+        // Write your code here
+        long right = arr.stream().reduce(0, (sum,  x) -> sum += x);
+        long left = 0;
+        int splits = 0;
+        for(int i = 0; i < (arr.size() - 1); i++){
+            int current = arr.get(i);
+            left += current;
+            right -= current;
+            if(left > right) splits++;
+        }
+
+        return splits;
+    }
+
+    public static void main(String[] args) {
+        int sum = splitIntoTwo(Arrays.asList(-3, -2, 10, 20, -30));
+        System.out.println(sum);
+    }
+
+}
---
+++ src/main/resources/bloodTypes.sql
@@ -0,0 +1,45 @@
+CREATE TABLE donor (
+    did int NOT NULL,
+    name varchar(20) NOT NULL,
+    gender char(1) NOT NULL,
+    city varchar(20) NOT NULL,
+    bg varchar(3) NOT NULL,
+    amount int NOT NULL
+    );
+
+INSERT INTO donor (did, name, gender, city, bg, amount) VALUES
+    (2, 'maria', 'f', 'warne', 'ab+', 7),
+    (3, 'maria', 'f', 'xxx', 'ab+', 3),
+    (4, 'maria', 'm', 'xxx', 'a-', 6),
+    (5, 'maria', 'f', 'xxx', 'ab+', 9),
+    (1, 'maria', 'm', 'xxx', 'a+', 1);
+
+CREATE TABLE acceptor (
+    did int NOT NULL,
+    name varchar(20) NOT NULL,
+    gender char(1) NOT NULL,
+    city varchar(20) NOT NULL,
+    bg varchar(3) NOT NULL,
+    amount int NOT NULL
+);
+
+INSERT INTO acceptor (did, name, gender, city, bg, amount) VALUES
+    (2, 'maria', 'f', 'warne', 'a+', 9),
+    (3, 'maria', 'm', 'warne', 'ab+', 8),
+    (4, 'maria', 'm', 'warne', 'ab+', 3),
+    (5, 'maria', 'f', 'warne', 'a+', 1),
+    (1, 'maria', 'f', 'xxx', 'a+', 5);
+
+-- gender, bg, count
+-- f, a+, 3
+-- f, ab+, 3
+-- m, a+, 1
+-- f, a-, 1
+-- f, ab+, 2
+
+select gender, bg, count(*) from
+    (SELECT did, gender, bg FROM donor
+     UNION
+     SELECT did, gender, bg FROM acceptor) everyone
+group by gender, bg
+order by gender, bg
